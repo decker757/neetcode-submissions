1efb256ac5c0981a3ee47d7478437143de33dadf
Ernest Tan <134849903+decker757@users.noreply.github.com>
2026-08-07 01:35:09 +0800

    Add: validate-parentheses - submission-26

---
+++ Data Structures & Algorithms/validate-parentheses/submission-26.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+
+        hm = {
+                ")": "(",
+                "]": "[",
+                "}": "{",
+            }
+
+        for ch in s:
+            if ch == "(" or ch == "[" or ch == "{":
+                st.append(ch)
+            
+            elif ch in hm and st:
+                if st.pop() != hm[ch]:
+                    return False
+            else:
+                return False  
+            
+        return True if len(st) == 0 else False
