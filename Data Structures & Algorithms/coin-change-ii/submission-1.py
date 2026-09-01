23654238d12e569c0d9c2cbb3fcd94d910f5b829
Ernest Tan <134849903+decker757@users.noreply.github.com>
2026-09-01 14:49:38 +0800

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        coins.sort()
+        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
+
+        def dfs(i, a):
+            if i >= len(coins):
+                return 0
+            if a == 0:
+                return 1
+            if memo[i][a] != -1:
+                return memo[i][a]
+            
+            res = 0
+            if a >= coins[i]:
+                res = dfs(i + 1, a)
+                res += dfs(i, a - coins[i])
+            memo[i][a] = res
+            return memo[i][a]
+        
+        return dfs(0, amount)
