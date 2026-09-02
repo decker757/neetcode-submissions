022d68bdabbf52f60a23f8bf78f1ef14858e1f24
Ernest Tan <134849903+decker757@users.noreply.github.com>
2026-09-02 15:06:15 +0800

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        hm1, hm2, l = {}, {}, 0
+
+        for i in range(len(s1)):
+            hm1[s1[i]] = hm1.get(s1[i], 0) + 1
+            hm2[s2[i]] = hm2.get(s2[i], 0) + 1
+
+        matches = 0
+
+        for k in hm1:
+            if hm1[k] == hm2.get(k, 0):
+                matches += 1
+
+        for r in range(len(s1), len(s2)):
+            if len(hm1) == matches:
+                return True
+            
+            ch = s2[r]
+            hm2[ch] = hm2.get(ch, 0) + 1
+
+            if ch in hm1 and hm2[ch] == hm1[ch]:
+                matches += 1
+            elif ch in hm1 and hm1[ch] + 1 == hm2[ch]:
+                matches -= 1
+
+            ch = s2[l]
+            hm2[ch] = hm2.get(ch, 0) - 1
+            if ch in hm1 and hm2[ch] == hm1[ch]:
+                matches += 1
+            elif ch in hm1 and hm1[ch] - 1 == hm2[ch]:
+                matches -= 1
+            
+            l += 1
+        return matches == len(hm1)
+        
+        
