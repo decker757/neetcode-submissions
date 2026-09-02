e12765a927519bc7318e3b5a2ab04396dac710c5
Ernest Tan <134849903+decker757@users.noreply.github.com>
2026-09-02 19:44:03 +0800

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resStartingIdx = 0
+        resLen = 0
+        def expand(l, r):
+            nonlocal resStartingIdx, resLen
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > resLen:
+                    resLen = r - l + 1
+                    resStartingIdx = l
+                l -= 1
+                r += 1
+            
+        for i in range(len(s)):
+            expand(i, i)
+            expand(i, i + 1)
+        
+        return s[resStartingIdx: resStartingIdx + resLen]
