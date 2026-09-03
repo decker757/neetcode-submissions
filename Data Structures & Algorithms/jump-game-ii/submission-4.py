3834301d1e75aecec5dd019fb99006783d566851
Ernest Tan <134849903+decker757@users.noreply.github.com>
2026-09-03 17:55:48 +0800

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        max_reach = 0
+        curr = 0
+        res = 0
+
+        for i in range(len(nums) - 1):
+            max_reach = max(max_reach, nums[i] + i)
+            if i == curr:
+                res += 1
+                curr = max_reach
+
+        return res
+            
+
